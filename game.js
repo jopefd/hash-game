453485982b6ecf1cf51af1d4b54a8a8989d2436b
joao pedro <fonsecajope@gmail.com>
2020-09-02 21:09:25 -0300

    Create inicializarEspacos(), and modify atualizaMostrador()

--- game.js
+++ game.js
@@ -4,19 +4,41 @@ let playTime = player1;
 let gameOver = false;
 
 atualizaMostrador();
+inicializarEspacos();
 
 function atualizaMostrador() {
 	if (gameOver) {
 		return;
 	}
 	if(playTime == player1) {
-		let player = document.querySelectorAll("div#mostrador img")[0];
-		player.setAttribute("src", "https://img.icons8.com/small/32/000000/delete-sign.png");
-		player.setAttribute("alt", "xis");
+		let player = document.getElementById("x-or-o");
+		player.innerHTML = `<div id="x"><div id="x-slash-1"></div><div id="x-slash-2"></div></div>`;
+	} else {
+		let player = document.getElementById("x-or-o");
+		player.innerHTML = `<div id='o'></div>`;
 	}
-	if(playTime == player2) {
-		let player = document.querySelectorAll("div#mostrador img")[0];
-		player.setAttribute("src", "https://img.icons8.com/small/32/000000/unchecked-circle.png");
-		player.setAttribute("alt", "bola")
+}
+
+function inicializarEspacos() {
+	let espacos = document.querySelector(".tabuleiro").children;
+	for (var i = 0; i < espacos.length; i++) {
+		espacos[i].addEventListener("click", function() {
+			if(gameOver){
+				return;
+			}
+
+			if(this.children.length == 0) {
+				if(playTime == player1) {
+					this.innerHTML = `<div id="x"><div id="x-slash-1"></div><div id="x-slash-2"></div></div>`;
+					this.setAttribute("jogada", player1);
+					playTime = player2;
+				} else {
+					this.innerHTML = `<div id='o'></div>`;
+					this.setAttribute("jogada", player2);
+					playTime = player1;
+				}
+			}
+			atualizaMostrador();
+		});
 	}
 }
